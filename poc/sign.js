0f5fe9849ec2eb93678a703b500e468b4d882b2a
Cody Django Redmond <codydjango@gmail.com>
2019-03-10 17:30:29 -0700

    refactor to use exports

--- poc/sign.js
+++ poc/sign.js
@@ -1,20 +1,39 @@
 const sodium = require('sodium-native')
-const prompt = require('../utilities/prompt')
+const prompt = require('./prompt')
+const runAsCli = (require.main.filename === module.filename)
 
-
-;(async () => {
+function createKeypair() {
     const publicKeyBuf = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
     const secretKeyBuf = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
-    const signatureBuf = Buffer.alloc(sodium.crypto_sign_BYTES)
 
     sodium.crypto_sign_keypair(publicKeyBuf, secretKeyBuf)
 
-    const message = await prompt('Message: ')
-    const messageBuf = Buffer.from(message)
+    return {
+        public: publicKeyBuf.toString('hex'),
+        secret: secretKeyBuf.toString('hex')
+    }
+}
 
-    sodium.crypto_sign_detached(signatureBuf, messageBuf, secretKeyBuf)
+function sign(message, secret) {
+    const secretBuf = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES, Buffer.from(secret, 'hex'))
+    const messageBuf = Buffer.from(message)
+    const signatureBuf = Buffer.alloc(sodium.crypto_sign_BYTES)
+
+    sodium.crypto_sign_detached(signatureBuf, messageBuf, secretBuf)
+
+    return signatureBuf.toString('hex')
+}
+
+async function test() {
+    const { public, secret } = createKeypair()
+    const message = await prompt('Message: ')
+    const signature = sign(message, secret)
 
     console.log(`Signed message: ${ message }`)
-    console.log(`Public Key: ${ publicKeyBuf.toString('hex') }`)
-    console.log(`Signature: ${ signatureBuf.toString('hex') }`)
-})()
+    console.log(`Public Key: ${ public }`)
+    console.log(`Signature: ${ signature }`)
+}
+
+if (runAsCli) test()
+
+module.exports = sign
--- poc/verify.js
+++ poc/verify.js
@@ -1,15 +1,24 @@
 const sodium = require('sodium-native')
-const prompt = require('../utilities/prompt')
+const prompt = require('./prompt')
+const runAsCli = (require.main.filename === module.filename)
 
-;(async () => {
-    const publicKey = await prompt('Public Key: ')
-    const message = await prompt('Message: ')
-    const signature = await prompt('Signature: ')
-
+function verify(signature, message, public) {
     const messageBuf = Buffer.from(message)
     const signatureBuf = Buffer.from(signature, 'hex')
-    const publicKeyBuf = Buffer.from(publicKey, 'hex')
-    const verified = sodium.crypto_sign_verify_detached(signatureBuf, messageBuf, publicKeyBuf)
+    const publicBuf = Buffer.from(public, 'hex')
+
+    return sodium.crypto_sign_verify_detached(signatureBuf, messageBuf, publicBuf)
+}
+
+async function test() {
+    const signature = await prompt('Signature: ')
+    const message = await prompt('Message: ')
+    const public = await prompt('Public Key: ')
+    const verified = verify(signature, message, public)
 
     console.log(`verified ${ verified }`)
-})()
+}
+
+if (runAsCli) test()
+
+module.exports = verify
