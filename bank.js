1805d0bfd3b4c7ba900f2e822908bb5f55884f09
Cody Django Redmond <codydjango@gmail.com>
2019-03-12 15:00:15 -0700

    add replay protoection

--- bank.js
+++ bank.js
@@ -1,15 +1,17 @@
-// const jsonStream = require('duplex-json-stream')
+// bank.js
+
 const JsonSocket = require('json-socket')
 const net = require('net')
 const Log = require('./classes/Log')
 const Bank = require('./classes/Bank')
 const verify = require('./tools/verify')
 
-const reset = true
+// constants
+const RESET = false
+const SUPPORTED_COMMANDS = ['register', 'withdraw', 'balance', 'deposit']
 
-const log = new Log(reset)
-// const log = new SecureLog({ reset, keypairStore, secretStore })
-const bank = new Bank(determineBalanceFromLog(log))
+const log = new Log(RESET)
+const bank = new Bank(determineBalancesFromLog(log))
 
 // Register methods to record
 log.logify(bank, ['register', 'withdraw', 'deposit'])
@@ -25,41 +27,79 @@ function handleConnection(socket) {
 
 // Route the message passed through JsonSocket
 function handleMessage(socket, msg) {
-    // Validations
-    const hasCustomerId = msg => msg.customerId !== undefined
-    const hasValidSignature = ({ signature, ...msg }) => verify(signature, JSON.stringify(msg), msg.customerId)
 
-    // Responses
-    const pong = msg => socket.sendMessage({ msg: 'pong' })
-    const reject = ({ customerId, cmd }, error) => socket.sendMessage({ customerId, cmd, error })
-    const balance = ({ customerId, cmd }) => socket.sendMessage({ customerId, cmd, balance: bank.getBalance(customerId) })
+    // Declare validations
+    const hasCustomerId = ({ customerId }) => customerId !== undefined
+    const hasValidSignature = ({ signature, ...msg }) => verify(signature, JSON.stringify(msg), msg.customerId)
+    const hasValidCommand = ({ cmd }) => (SUPPORTED_COMMANDS.indexOf(cmd) !== -1)
+    const hasValidBestHash = ({ hash }) => (hash === log.bestHash)
+
+    // Default reject handler
+    const reject = ({ customerId, cmd }, error) => {
+        socket.sendMessage({
+            customerId,
+            cmd,
+            error
+        })
+    }
 
     // Check validations
-    if (!hasCustomerId(msg)) return reject('No customerId')
-    if (!hasValidSignature(msg)) return reject('Signature is not valid')
+    if (!hasCustomerId(msg)) return reject(msg, 'No customerId')
+    if (!hasValidSignature(msg)) return reject(msg, 'Signature is not valid')
+    if (!hasValidCommand(msg)) return reject(msg, 'Command not supported')
 
-    // Route message
+    // Route messaging
     switch (msg.cmd) {
         case 'register':
-            (bank.canRegister(msg)) ? bank.register(msg) : reject(msg, "Register request is invalid.")
-            return balance(msg)
+            if (!bank.canRegister(msg)) return reject(msg, "Register request is invalid.")
+
+            bank.register(msg)
+            socket.sendMessage({
+                customerId: msg.customerId,
+                cmd: msg.cmd,
+                balance: bank.getBalance(msg.customerId),
+                hash: log.bestHash
+            })
+            break
         case 'deposit':
-            (bank.canDeposit(msg)) ? bank.deposit(msg) : reject(msg, "Deposit request is invalid.")
-            return balance(msg)
+            if (!hasValidBestHash(msg)) return reject(msg, "Replay request")
+            if (!bank.canDeposit(msg)) return reject(msg, "Deposit request is invalid.")
+
+            bank.deposit(msg)
+            socket.sendMessage({
+                customerId: msg.customerId,
+                cmd: msg.cmd,
+                balance: bank.getBalance(msg.customerId),
+                hash: log.bestHash
+            })
+            break
         case 'withdraw':
-            (bank.canWithdraw(msg)) ? bank.withdraw(msg) : reject(msg, "Withdrawal request is invalid.")
-            return balance(msg)
+            if (!hasValidBestHash(msg)) return reject(msg, "Replay request")
+            if (!bank.canWithdraw(msg)) return reject(msg, "Withdrawal request is invalid.")
+
+            bank.withdraw(msg)
+            socket.sendMessage({
+                customerId: msg.customerId,
+                cmd: msg.cmd,
+                balance: bank.getBalance(msg.customerId),
+                hash: log.bestHash
+            })
+            break
         case 'balance':
-            return balance(msg)
-        case 'ping':
-            return pong()
+            socket.sendMessage({
+                customerId: msg.customerId,
+                cmd: msg.cmd,
+                balance: bank.getBalance(msg.customerId),
+                hash: log.bestHash
+            })
+            break
         default:
-            return reject('not supported')
+            break
     }
 }
 
-// Helper method to determine the log from the previous balance
-function determineBalanceFromLog(log) {
+// Helper method to determine the bank balances from the log.
+function determineBalancesFromLog(log) {
     return log.toArray().map(entry => entry.value).reduce((acc, current) => {
         switch (current.cmd) {
             case 'register':
--- teller.js
+++ teller.js
@@ -2,23 +2,35 @@
 const JsonSocket = require('json-socket')
 const net = require('net')
 
+const output = require('./tools/output')
 const generateKeypair = require('./tools/generateKeypair')
 const sign = require('./tools/sign')
 const socket = new JsonSocket(new net.Socket())
 
+const DEMONSTRATE_REPLAY = false
+
+let bestHash = ''
+
 socket
-    .on('message', m => console.log('response', JSON.stringify(m, null, 2)))
+    .on('message', message => {
+        output(JSON.stringify(message, null, 2), (message.error) ? 'red' : 'green')
+
+        if (message.hash && (bestHash !== message.hash)) {
+            output(`updating hash: ${ message.hash }`)
+            bestHash = message.hash
+        }
+    })
     .connect(3876)
+
 
 const customer1 = generateKeypair()
 const customer2 = generateKeypair()
 
 const requestQueue = [
-    [customer1, { cmd: 'ping' }],
     [customer1, { cmd: 'register'}],
     [customer1, { cmd: 'deposit', amount: 200 }],
     [customer2, { cmd: 'register'}],
-    [customer1, { cmd: 'deposit', amount: 123 }],
+    [customer1, { cmd: 'deposit', amount: 500 }],
     [customer1, { cmd: 'withdraw', amount: 11 }],
     [customer1, { cmd: 'withdraw', amount: 23 }],
     [customer1, { cmd: 'withdraw', amount: 3000 }],
@@ -30,12 +42,41 @@ const requestQueue = [
 ]
 
 function processRequest(customer, request) {
+    if (bestHash) request.hash = bestHash
     request.customerId = customer.public
     request.signature = sign(JSON.stringify(request), customer.secret)
+
+    if (DEMONSTRATE_REPLAY && request.cmd === 'deposit') {
+        setTimeout(()=> {
+            output(JSON.stringify(request, null, 2), 'pink')
+            socket.sendMessage(request)
+        }, 10)
+
+        setTimeout(()=> {
+            output(JSON.stringify(request, null, 2), 'pink')
+            socket.sendMessage(request)
+        }, 20)
+
+        setTimeout(()=> {
+            output(JSON.stringify(request, null, 2), 'pink')
+            socket.sendMessage(request)
+        }, 30)
+    }
+
+    output(JSON.stringify(request, null, 2))
     socket.sendMessage(request)
 }
 
+// I'm just using a 500ms timeout to give enough time for the server to respond with
+// an updated best hash. This is to avoid the replay threat. Using a timeout if kinda silly
+// but if we really wanted to take the trasactional nature seriously we
+// probably wouldn't be using sockets, either... I mean unless we were really looking
+// to blow this out...
 (function process(requestQueue) {
     processRequest(...requestQueue.shift())
-    if (requestQueue.length > 0) process(requestQueue)
+    if (requestQueue.length > 0) {
+        setTimeout(() => process(requestQueue), 500)
+    } else {
+        socket.end()
+    }
 })(requestQueue)
