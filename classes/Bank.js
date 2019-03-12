4c737d77110cf113b628a1697ad401f91a0f9bfb
Cody Django Redmond <codydjango@gmail.com>
2019-03-11 17:10:20 -0700

    more terse and robust

--- classes/Bank.js
+++ classes/Bank.js
@@ -1,11 +1,9 @@
 class Bank {
     static calculateBalancesFromLog(log) {
         return log.toArray().map(entry => entry.value).reduce((acc, current) => {
-            if (!Number.isInteger(acc[current.customerId])) {
-                acc[current.customerId] = 0
-            }
-
             switch (current.cmd) {
+                case 'register':
+                    acc[current.customerId] = 0
                 case 'deposit':
                     acc[current.customerId] += current.amount
                     break
@@ -21,13 +19,9 @@ class Bank {
     constructor({ log }) {
         this._balances = Bank.calculateBalancesFromLog(log)
 
-        this.register = this.register.bind(this)
-        this.deposit = this.deposit.bind(this)
-        this.withdraw = this.withdraw.bind(this)
-
-        this.register = log.constructor.logify(this.register, log)
-        this.deposit = log.constructor.logify(this.deposit, log)
-        this.withdraw = log.constructor.logify(this.withdraw, log)
+        this.register = log.constructor.logify(this.register.bind(this), log)
+        this.deposit = log.constructor.logify(this.deposit.bind(this), log)
+        this.withdraw = log.constructor.logify(this.withdraw.bind(this), log)
     }
 
     getBalance(customerId) {
@@ -46,16 +40,20 @@ class Bank {
         this._balances[customerId] -= amount
     }
 
-    canRegister({ customerId }) {
+    isRegistered(customerId) {
         return (this._balances[customerId] === undefined)
     }
 
-    canDeposit({ amount }) {
-        return (amount >= 0)
+    canRegister({ customerId }) {
+        return (!isRegistered(customerId))
+    }
+
+    canDeposit({ customerId, amount }) {
+        return this.isRegistered(customerId) && (amount >= 0)
     }
 
     canWithdraw({ customerId, amount }) {
-        return (this._balances[customerId] >= amount)
+        return this.isRegistered(customerId) && (this._balances[customerId] >= amount)
     }
 }
 
