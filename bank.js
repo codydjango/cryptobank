3bcfcaef20e6028ebd09e70069e1517694e5a77f
Cody Django Redmond <codydjango@gmail.com>
2019-03-12 12:23:34 -0700

    refactor teller and bank to use signatures and json-socket

--- bank.js
+++ bank.js
@@ -1,20 +1,64 @@
-const jsonStream = require('duplex-json-stream')
+// const jsonStream = require('duplex-json-stream')
+const JsonSocket = require('json-socket')
 const net = require('net')
 const Log = require('./classes/Log')
 const Bank = require('./classes/Bank')
+const verify = require('./tools/verify')
 
 const reset = true
 
 const log = new Log(reset)
+// const log = new SecureLog({ reset, keypairStore, secretStore })
 const bank = new Bank(determineBalanceFromLog(log))
 
+// Register methods to record
 log.logify(bank, ['register', 'withdraw', 'deposit'])
 
-net.createServer(socket => {
-    socket = jsonStream(socket)
-    socket.on('data', msg => handleMessage(socket, msg))
-}).listen(3876)
+// Bootstrap the server
+net.createServer().on('connection', handleConnection).listen(3876)
 
+// Handle the connection and upgrade the socket to JsonSocket
+function handleConnection(socket) {
+    socket = new JsonSocket(socket)
+    socket.on('message', msg => handleMessage(socket, msg))
+}
+
+// Route the message passed through JsonSocket
+function handleMessage(socket, msg) {
+    // Validations
+    const hasCustomerId = msg => msg.customerId !== undefined
+    const hasValidSignature = ({ signature, ...msg }) => verify(signature, JSON.stringify(msg), msg.customerId)
+
+    // Responses
+    const pong = msg => socket.sendMessage({ msg: 'pong' })
+    const reject = ({ customerId, cmd }, error) => socket.sendMessage({ customerId, cmd, error })
+    const balance = ({ customerId, cmd }) => socket.sendMessage({ customerId, cmd, balance: bank.getBalance(customerId) })
+
+    // Check validations
+    if (!hasCustomerId(msg)) return reject('No customerId')
+    if (!hasValidSignature(msg)) return reject('Signature is not valid')
+
+    // Route message
+    switch (msg.cmd) {
+        case 'register':
+            (bank.canRegister(msg)) ? bank.register(msg) : reject(msg, "Register request is invalid.")
+            return balance(msg)
+        case 'deposit':
+            (bank.canDeposit(msg)) ? bank.deposit(msg) : reject(msg, "Deposit request is invalid.")
+            return balance(msg)
+        case 'withdraw':
+            (bank.canWithdraw(msg)) ? bank.withdraw(msg) : reject(msg, "Withdrawal request is invalid.")
+            return balance(msg)
+        case 'balance':
+            return balance(msg)
+        case 'ping':
+            return pong()
+        default:
+            return reject('not supported')
+    }
+}
+
+// Helper method to determine the log from the previous balance
 function determineBalanceFromLog(log) {
     return log.toArray().map(entry => entry.value).reduce((acc, current) => {
         switch (current.cmd) {
@@ -31,27 +75,4 @@ function determineBalanceFromLog(log) {
                 return acc
         }
     }, {})
-}
-
-function handleMessage(socket, msg) {
-    if (msg.customerId === undefined) return reject('No customerId')
-
-    const reject = msg => { socket.write({ error: msg }) }
-    const balance = customerId => { socket.write({ balance: bank.getBalance(customerId) })}
-
-    switch (msg.cmd) {
-        case 'register':
-            (bank.canRegister(msg)) ? bank.register(msg) : reject("Register request is invalid.")
-            return balance(msg.customerId)
-        case 'deposit':
-            (bank.canDeposit(msg)) ? bank.deposit(msg) : reject("Deposit request is invalid.")
-            return balance(msg.customerId)
-        case 'withdraw':
-            (bank.canWithdraw(msg)) ? bank.withdraw(msg) : reject("Withdrawal request is invalid.")
-            return balance(msg.customerId)
-        case 'balance':
-            return balance(msg.customerId)
-        default:
-            return reject('not supported')
-    }
 }
--- teller.js
+++ teller.js
@@ -1,27 +1,41 @@
 // teller.js
-const jsonStream = require('duplex-json-stream')
+const JsonSocket = require('json-socket')
 const net = require('net')
 
-const client = jsonStream(net.connect(3876))
+const generateKeypair = require('./tools/generateKeypair')
+const sign = require('./tools/sign')
+const socket = new JsonSocket(new net.Socket())
 
-console.log('Start Teller')
+socket
+    .on('message', m => console.log('response', JSON.stringify(m, null, 2)))
+    .connect(3876)
 
-client.on('data', function (msg) {
-    console.log('Teller received:', msg)
-})
+const customer1 = generateKeypair()
+const customer2 = generateKeypair()
 
-const customer1 = 'first'
-const customer2 = 'second'
+const requestQueue = [
+    [customer1, { cmd: 'ping' }],
+    [customer1, { cmd: 'register'}],
+    [customer1, { cmd: 'deposit', amount: 200 }],
+    [customer2, { cmd: 'register'}],
+    [customer1, { cmd: 'deposit', amount: 123 }],
+    [customer1, { cmd: 'withdraw', amount: 11 }],
+    [customer1, { cmd: 'withdraw', amount: 23 }],
+    [customer1, { cmd: 'withdraw', amount: 3000 }],
+    [customer1, { cmd: 'withdraw', amount: 10 }],
+    [customer1, { cmd: 'withdraw', amount: 1000 }],
+    [customer1, { cmd: 'deposit', amount: 231 }],
+    [customer1, { cmd: 'balance' }],
+    [customer1, { cmd: 'balance' }]
+]
 
-client.write({ customerId: customer1, cmd: 'register', })
-client.write({ customerId: customer1, cmd: 'deposit', amount: 200 })
-client.write({ customerId: customer2, cmd: 'register', })
-client.write({ customerId: customer2, cmd: 'deposit', amount: 123 })
-client.write({ customerId: customer1, cmd: 'withdraw', amount: 11 })
-client.write({ customerId: customer1, cmd: 'withdraw', amount: 23 })
-client.write({ customerId: customer1, cmd: 'withdraw', amount: 3000 })
-client.write({ customerId: customer2, cmd: 'withdraw', amount: 10 })
-client.write({ customerId: customer2, cmd: 'withdraw', amount: 1000 })
-client.write({ customerId: customer2, cmd: 'deposit', amount: 231 })
-client.write({ customerId: customer1, cmd: 'balance' })
-client.write({ customerId: customer2, cmd: 'balance' })
+function processRequest(customer, msg) {
+    msg.customerId = customer.public
+    msg.signature = sign(JSON.stringify(msg), customer.secret)
+    socket.sendMessage(msg)
+}
+
+(function process(requestQueue) {
+    processRequest(...requestQueue.shift())
+    if (requestQueue.length > 0) process(requestQueue)
+})(requestQueue)
